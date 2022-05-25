f59108d5a3b6e4db15398971fe380aed392478a4
Alexander Herfurtner <alexander.h@variscite.com>
2022-05-25 21:29:14 +0200

    Merge of SDK_2_11_1_EVK_MIMX8MN
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

---
+++ docs/MCUXpresso SDK API Reference Manual_MIMX8MN6/a00126.js
@@ -0,0 +1,6 @@
+var a00126 =
+[
+    [ "CODEC Common Driver", "a00011.html", "a00011" ],
+    [ "CODEC I2C Driver", "a00127.html", null ],
+    [ "WM8524 Driver", "a00030.html", "a00030" ]
+];
---
+++ docs/MCUXpresso SDK API Reference Manual_MIMX8MN6/search/all_6f.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['osc24m_5fclk_5ffreq',['OSC24M_CLK_FREQ',['../a00010.html#gae6d3ae23f8c4f25851e3311531aa20d0',1,'fsl_clock.h']]],
+  ['outdataaddr',['outDataAddr',['../a00008.html#ab2038ba02b5fd5c56be730e515c03283',1,'asrc_transfer_t']]],
+  ['outdatasize',['outDataSize',['../a00008.html#a19173474808ea73d53201508eddbbaad',1,'asrc_transfer_t']]],
+  ['outdmahandle',['outDMAHandle',['../a00009.html#a800b95299bc6481c2b3d91b9838da76f',1,'_asrc_sdma_handle']]],
+  ['outputlogic',['outputLogic',['../a00014.html#a9d37ffd9a2943f10a91095759bd52da5',1,'gpio_pin_config_t']]]
+];
